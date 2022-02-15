e5cd833fd4e52b40690c1b23e6196e9ebbd8ddf7
damilola <oyeyipo45@gmail.com>
2022-02-15 13:30:52 +0100

    update

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,27 +1,20 @@
 import React, { useEffect } from 'react';
-import './modal.css';
 
 const Modal = (props) => {
-//   const closeOnEscapeKeyDown = (e) => {
-//     if ((e.charCode || e.keyCode) === 27) {
-//       props.onClose();
-//     }
-//   };
+  const closeOnEscapeKeyDown = (e) => {
+    if ((e.charCode || e.keyCode) === 27) {
+      props.onClose();
+    }
+  };
 
-//   useEffect(() => {
-//     document.body.addEventListener('keydown', closeOnEscapeKeyDown);
-//     return function cleanup() {
-//       document.body.removeEventListener('keydown', closeOnEscapeKeyDown);
-//     };
-//   }, []);
 
   return (
       <div className='modal' >
         <div className='modal-content' >
           <div className='modal-header'>
-            <h4 className='modal-title'> modal title</h4>
+            <h4 className='modal-title'> picture</h4>
           </div>
-          <div className='modal-body'>this is the modal content</div>
+          <div className='modal-body'>artist name</div>
           <div className='modal-footer'>
             <button  className='button'>
               Close
